33d7f5d334085e5df74b5b0622654e259bac5a54
lesMan224 <leslymileng@gmail.com>
2025-09-24 23:08:15 +0200

    Added rsvp api route to server

--- src/server.js
+++ src/server.js
@@ -36,6 +36,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/events', eventRoutes);
 app.use('/api', reviewRoutes);  // nested under events
 app.use('/api/admin', adminRoutes);
+app.use('/api/events', rsvpRoutes);
 
 // Default route
 app.get('/', (req, res) => {
