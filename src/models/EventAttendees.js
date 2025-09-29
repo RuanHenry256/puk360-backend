334f30f4cd822f525974d068bc40ed410c8d001b
lesMan224 <leslymileng@gmail.com>
2025-09-29 22:21:37 +0200

    fixed api points

--- src/models/EventAttendees.js
+++ src/models/EventAttendees.js
@@ -1,5 +1,6 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/db.js";
+import User from "./User.js";
 
 const Event_Attendees = sequelize.define("Event_Attendees", {
     Event_ID: { type: DataTypes.INTEGER, primaryKey: true, allowNull: false, references: { model: 'Event', key: 'Event_ID'}},
@@ -9,12 +10,15 @@ const Event_Attendees = sequelize.define("Event_Attendees", {
 }, {
     tableName: 'Event_Attendees', //Explicit table name
     timestamps: false, // Since there is already own Timestamp field
-    Indexes: [
+    indexes: [
         {
             unique: true,
             fields: ['Event_ID', 'User_ID'] // composite primary key
         } 
     ]
 });
+
+
+
 
 export default Event_Attendees;
